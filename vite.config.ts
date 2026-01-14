da68acc72e7387619391b534ae3fae4527043b51
Ericson Asamoah <your-github-email@example.com>
2026-01-14 07:29:37 +0000

    Fix Vite base for Amplify and Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  base: "/IDFinder/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "ghpages" ? "/IDFinder/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
-
+}));
