6abe20d65f423672ac0341dd1f30ed526dfdcaad
Ericson Asamoah <your-github-email@example.com>
2026-02-24 19:50:47 +0000

    CC3push

--- src/cognitoConfig.ts
+++ src/cognitoConfig.ts
@@ -9,8 +9,8 @@ Amplify.configure({
         oauth: {
           domain: "idfinder-app.auth.eu-north-1.amazoncognito.com",
           scopes: ["openid", "email"],
-          redirectSignIn: ["http://localhost:5173", "https://d84l1y8p4kdic.cloudfront.net"],
-          redirectSignOut: ["http://localhost:5173", "https://d84l1y8p4kdic.cloudfront.net"],
+          redirectSignIn: ["http://localhost:5173", "https://d84l1y8p4kdic.cloudfront.net","https://master.dfkf39t8knl1f.amplifyapp.com"],
+          redirectSignOut: ["http://localhost:5173", "https://d84l1y8p4kdic.cloudfront.net","https://master.dfkf39t8knl1f.amplifyapp.com"],
           responseType: "code",
         },
       },
